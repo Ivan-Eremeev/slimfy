0242d3cc18d76ac25ed330e216cfadc9c9ef27fa
Ivan <ivan.frontcoder@gmail.com>
2022-06-17 13:00:07 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -65,11 +65,4 @@ $(document).ready(function () {
 	}
 	chartAnimate();
 
-	// Высота переменной равная внутренней высоте окна, без тулбара на мобилках
-	window.addEventListener('resize', () => {
-		// We execute the same script as before
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	});
-
 });
